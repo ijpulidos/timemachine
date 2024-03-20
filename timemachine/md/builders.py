ccf69403cefcfa117d529c3dd7526d978a905e2e
Iván Pulido <2949729+ijpulidos@users.noreply.github.com>
2024-03-19 21:53:34 -0400

    Adding support for phosaa10 ff. Docstring.

--- timemachine/md/builders.py
+++ timemachine/md/builders.py
@@ -11,18 +11,54 @@ def strip_units(coords):
     return np.array(coords.value_in_unit_system(unit.md_unit_system))
 
 
-def build_protein_system(host_pdbfile: Union[app.PDBFile, str], protein_ff: str, water_ff: str):
+def build_protein_system(host_pdbfile: Union[app.PDBFile, str], protein_ff: str, water_ff: str, phosaa_ff: str = "amber/phosaa10"):
     """
-    Build a solvated protein system with a 10A padding.
+    Build a solvated protein system.
 
     Parameters
-    ---------
-    host_pdbfile: str or app.PDBFile
-        PDB of the host structure
+    ----------
+    host_pdbfile : Union[app.PDBFile, str]
+        The PDB file or path to the PDB file of the host protein.
+    protein_ff : str
+        The force field parameters for the protein.
+    water_ff : str
+        The force field parameters for water.
+    phosaa_ff : str, optional
+        The force field parameters for phosphorous amino acids (default is "amber/phosaa10").
+        Note that "amber/phosaa10.xml" requires `openmmforcefields` to be installed in the env.
 
+    Returns
+    -------
+    tuple
+        A tuple containing the following elements:
+        - solvated_host_system : openmm.System
+            The solvated protein system.
+        - solvated_host_coords : numpy.ndarray
+            The coordinates of the solvated protein system.
+        - box : numpy.ndarray
+            The box dimensions of the solvated protein system.
+        - topology : openmm.Topology
+            The topology of the solvated protein system.
+        - nwa : int
+            The number of water atoms in the solvated protein system.
+
+    Raises
+    ------
+    TypeError
+        If host_pdbfile is neither a string nor an openmm PDBFile object.
+
+    Notes
+    -----
+    This function builds a solvated protein system using the provided PDB file and force field parameters.
+    It adds solvent molecules around the protein using the specified force field parameters for protein,
+    water, and optionally phosphorous amino acids.
+
+    Example
+    -------
+    solvated_host_system, solvated_host_coords, box, topology, nwa = build_protein_system(
+        "host.pdb", "amber14/protein.ff14SB", "amber14/tip3p", phosaa_ff="amber/phosaa10")
     """
-
-    host_ff = app.ForceField(f"{protein_ff}.xml", f"{water_ff}.xml")
+    host_ff = app.ForceField(f"{protein_ff}.xml", f"{water_ff}.xml", f"{phosaa_ff}.xml")
     if isinstance(host_pdbfile, str):
         assert os.path.exists(host_pdbfile)
         host_pdb = app.PDBFile(host_pdbfile)
